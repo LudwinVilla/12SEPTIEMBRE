442eefe0678afaf9c93bc08d6adf894f996e14be
LudwinVilla <110070989+LudwinVilla@users.noreply.github.com>
2022-10-24 19:15:00 -0600

    comiit

--- index.php
+++ index.php
@@ -5,14 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-    <a href="VistaEstudiante.php">Ir a Vista Estudiante</a>
+    <div>
+    <div class="contenido">
+        <div class="imagen">
+            <p> 
+                <img src="imagen.png"    alt="Muesta imagen DW">
+            </p>
+        </div>
+        <div class="derecho"> 
+            <h1 class="titulo">BIENVENIDO</h1>
+        </div>
+    </div>
+    </div>
+    <a href="VistaEstudiante.php">Deseo Inscribirme</a>
 
 
 
     <p>Ludwin Villatoro....Prueba de Sincronización</p>
 
+    </div>
+    
     
 
 
