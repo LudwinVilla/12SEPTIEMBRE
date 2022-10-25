39ee4c3e66600ec0105732ace61ac5f8f817af2b
LudwinVilla <110070989+LudwinVilla@users.noreply.github.com>
2022-10-24 21:01:08 -0600

    o

--- VistaEditarEstudiante.php
+++ VistaEditarEstudiante.php
@@ -4,56 +4,52 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/a87f2bfe07.js" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/a87f2bfe07.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="estilos.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"><script src="https://kit.fontawesome.com/a87f2bfe07.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-   
+    <title>VistaEditarEstudiante</title>
 </head>
 <body>
-    <div class="container">
-    <p>
-         <center> <h2>INGRESE SUS DATOS AQUI</h2></center>
-    </p>
-    <center><div class="contenedor">
-    <form action="EstudianteController.php" method="post">
-   
-    <p>
-         <div class="col-4">
-        <label for="apellidos">Nombre</label> 
-        <input type="text" class="form-control" name="apellidos" placeholder="Ingresa tu nombre">
-        </p>
-    </div>
+    <form action="EstudianteController.php" method="POST">
+                        <?php
+                            include_once "EstudianteModel.php";
+                            $nuevoEstudiante = new Estudiante();
+                            /*FILTRAR AL ESTUDIANTE SEGUN ID ENVIADO*/
+                            $resultado = $nuevoEstudiante->FiltrarEstudiante($_GET['idEst']);
 
-        
-         <p>
-        <div class="col-4">
-        <label for="nombre">Apellidos </label>
-        <input type="text" class="form-control" name="nombre" placeholder="Ingresa tus Apellidos">
-        </p>
-        </div>
+                            while($resultadoFiltrado = mysqli_fetch_assoc($resultado))
+                            {
+                        ?>
+                                <p>
+                                <label for="txtapellidos">Apellidos:</label>
+                                <input type="text" name="txtapellidos" 
+                                value="<?php echo $resultadoFiltrado['apellidos']?>">
+                                </p>
 
-        <p>
-            <div class="col-4">
-            <label for="direccion">Email</label>
-            <input type="text" class="form-control" name="direccion" placeholder="Ingresa tu Direccion Email">
-        </p>
-        </div>
-        
-          <p>
-            <div class="col-4">
-            <label for="telefono">Telefono</label>
-            <input type="number" class="form-control" name="telefono" placeholder="Ingresa tu Telefono">
-        </p>
-        </div>
-        
-        
-           <p>
-            <input type="submit" value="Enviar Datos">
-        </p>
-        </div>     
-        </center>  
-        </div>  
+                                <p>
+                                <label for="txtnombre">Nombre:</label>
+                                <input type="text" name="txtnombre" 
+                                value="<?php echo $resultadoFiltrado['nombre']?>">
+                                </p>
+
+                                <p>
+                                <label for="txtdireccion">Direccion:</label>
+                                <input type="text" name="txtdireccion" 
+                                value="<?php echo $resultadoFiltrado['direccion']?>">
+                                </p>
+
+                                <p>
+                                <label for="txttelefono">Telefono:</label>
+                                <input type="text" name="txttelefono" 
+                                value="<?php echo $resultadoFiltrado['telefono']?>">
+                                </p>
+
+                                 <p>
+                                    <input type="hidden" name="idStudent" 
+                                    value="<?php echo $resultadoFiltrado['idEstudiante']?>">
+                                 </p>   
+                            <?php
+                            }
+                            ?>
+                        
+                <input type="submit" value="Editar Estudiante" name="btnEditar">            
+    </form>
 </body>
 </html>
--- VistaEstudiante.php
+++ VistaEstudiante.php
@@ -5,65 +5,55 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/a87f2bfe07.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/a87f2bfe07.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="estilos.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"><script src="https://kit.fontawesome.com/a87f2bfe07.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-    <title>Document</title>
+   
 </head>
 <body>
+    <div class="container">
     <p>
-        <h2>INGRESO DE DATOS DEL ESTUDIANTE</h2>
+         <center> <h2>INGRESE SUS DATOS AQUI</h2></center>
     </p>
+    <center><div class="contenedor">
     <form action="EstudianteController.php" method="post">
-        <p>
-            <label for="apellidos">Apellidos</label> apellidos
-            <input type="text" name="apellidos">
+   
+    <p>
+         <div class="col-4">
+        <label for="apellidos">Nombre</label> 
+        <input type="text" class="form-control" name="apellidos" placeholder="Ingresa tu nombre">
         </p>
+    </div>
+
         
-        <p>
-            <label for="nombre">Nombre</label>
-        <input type="text" name="nombre">
+         <p>
+        <div class="col-4">
+        <label for="nombre">Apellidos </label>
+        <input type="text" class="form-control" name="nombre" placeholder="Ingresa tus Apellidos">
         </p>
-        
+        </div>
+
         <p>
-            <label for="direccion">direccion</label>
-            <input type="text" name="direccion">
+            <div class="col-4">
+            <label for="direccion">Email</label>
+            <input type="text" class="form-control" name="direccion" placeholder="Ingresa tu Direccion Email">
         </p>
+        </div>
         
-        <p>
+          <p>
+            <div class="col-4">
             <label for="telefono">Telefono</label>
-            <input type="number" name="telefono">
+            <input type="number" class="form-control" name="telefono" placeholder="Ingresa tu Telefono">
         </p>
+        </div>
         
-        <p>
-            <input type="submit" value="Guardar Registro" name="btnGuardar">
+        
+           <p>
+            <input type="submit" value="Enviar Datos">
         </p>
-    </form>
-    <hr>
-    <h3>LISTADO DE ESTUDIANTES</h3>
-    <hr>
-    <table class="table table-dark table-striped">
-        <th>NOMBRE</th>
-        <th>APELLIDOS</th>
-        <th>DIRECCION</th>
-        <th>TELEFONO</th>
-        <th colspan="2">OPERACIONES</th>
-    <?php
-        include_once "EstudianteModel.php";
-        $Estudiante = new Estudiante();
-        $ListaEstudiantes = $Estudiante->ListarEstudiantes();
-        while($Estudiantes = mysqli_fetch_assoc($ListaEstudiantes))
-        {?>
-          <tr>
-                <td>  <?php echo $Estudiantes['nombre'] ?> </td>
-                <td>  <?php echo $Estudiantes['apellidos'] ?> </td>
-                <td>  <?php echo $Estudiantes['direccion'] ?> </td>
-                <td>  <?php echo $Estudiantes['telefono'] ?> </td>
-                <td><a href="VistaEditarEstudiante.php?idEst=<?php echo $Estudiantes['idEstudiante'];?>">EDITAR</a></td>
-                <td><a href="VistaEliminarEstudiante.php?idEst=<?php echo $Estudiantes['idEstudiante'];?>">Eliminar</a></td>
-          </tr>
-          
-       <?php } ?>
-    
-    </table>
+        </div>     
+        </center>  
+        </div>  
 </body>
 </html>
